8ba6b00deca9545d0dcbbda9ca3bca40bd7d4472
Danny Bui <dannybui5155@gmail.com>
2023-11-16 14:26:50 -0600

    working upload

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 # Import packages
 import base64
+import os
 
 from dash import Dash, html, dash_table, dcc, callback, Output, Input
+import dash
+from urllib.parse import quote as urlquote
+from flask import Flask, send_from_directory
 import pandas as pd
 import plotly.express as px
 import dash_mantine_components as dmc
@@ -10,55 +14,90 @@ from common import read_multiple_slices, processSlices, NominalHafx, read_time_s
 # Incorporate data
 df = pd.read_csv('gapminder2007.csv')
 
+UPLOAD_DIRECTORY = "/Users/dannybui/PycharmProjects/SSR-Data-Visualization/upload"
+
+if not os.path.exists(UPLOAD_DIRECTORY):
+    os.makedirs(UPLOAD_DIRECTORY)
+
 # Initialize the app - incorporate a Dash Mantine theme
 external_stylesheets = [dmc.theme.DEFAULT_COLORS]
-app = Dash(__name__, external_stylesheets=external_stylesheets)
+server = Flask(__name__)
+app = dash.Dash(server=server, external_stylesheets=external_stylesheets)
 
 # App layout
-app.layout = dmc.Container([
-    dmc.Title('Small Sat Data Dashboard', color="blue", size="h3"),
-    dcc.Upload(
-        id='upload-data',
-        children=html.Div([
-            'Drag and Drop or ',
-            html.A('Select Files')
-        ]),
-        style={
-            'width': '100%',
-            'height': '60px',
-            'lineHeight': '60px',
-            'borderWidth': '1px',
-            'borderStyle': 'dashed',
-            'borderRadius': '5px',
-            'textAlign': 'center',
-            'margin': '10px'
-        },
-        # Allow multiple files to be uploaded
-        multiple=True
-    ),
-], fluid=True)
+@server.route("/download/<path:path>")
+def download(path):
+    """Serve a file from the upload directory."""
+    return send_from_directory(UPLOAD_DIRECTORY, path, as_attachment=True)
 
-def parse_contents(contents, filename, date):
-    content_type, content_string = contents.split(',')
 
-    decoded = base64.b64decode(content_string)
-
-    if filename.endswith(".bin"):
-
-        slices = read_time_slices(filename)
-        df = pd.DataFrame(processSlices(slices))
-
-# Add controls to build the interaction
-@callback(
-    Output(component_id='graph-placeholder', component_property='figure'),
-    Input(component_id='my-dmc-radio-item', component_property='value')
+app.layout = dmc.Container(
+    [
+        dmc.Title("Small Sat Dashboard", color="blue", size="h3"),
+        dcc.Upload(
+            id="upload-data",
+            children=html.Div(
+                ["Drag and drop or click to select a file to upload."]
+            ),
+            style={
+                "width": "100%",
+                "height": "60px",
+                "lineHeight": "60px",
+                "borderWidth": "1px",
+                "borderStyle": "dashed",
+                "borderRadius": "5px",
+                "textAlign": "center",
+                "margin": "10px",
+            },
+            multiple=True,
+        ),
+        html.H2("File List"),
+        html.Ul(id="file-list"),
+    ],
+   fluid=True
 )
-def update_graph(col_chosen):
-    fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
-    return fig
-
-# Run the App
-if __name__ == '__main__':
-    app.run(debug=True)
 
 
+def save_file(name, content):
+    """Decode and store a file uploaded with Plotly Dash."""
+    data = content.encode("utf8").split(b";base64,")[1]
+    with open(os.path.join(UPLOAD_DIRECTORY, name), "wb") as fp:
+        fp.write(base64.decodebytes(data))
+
+
+def uploaded_files():
+    """List the files in the upload directory."""
+    files = []
+    for filename in os.listdir(UPLOAD_DIRECTORY):
+        path = os.path.join(UPLOAD_DIRECTORY, filename)
+        if os.path.isfile(path):
+            files.append(filename)
+    return files
+
+
+def file_download_link(filename):
+    """Create a Plotly Dash 'A' element that downloads a file from the app."""
+    location = "/download/{}".format(urlquote(filename))
+    return html.A(filename, href=location)
+
+
+@app.callback(
+    Output("file-list", "children"),
+    [Input("upload-data", "filename"), Input("upload-data", "contents")],
+)
+def update_output(uploaded_filenames, uploaded_file_contents):
+    """Save uploaded files and regenerate the file list."""
+
+    if uploaded_filenames is not None and uploaded_file_contents is not None:
+        for name, data in zip(uploaded_filenames, uploaded_file_contents):
+            save_file(name, data)
+
+    files = uploaded_files()
+    if len(files) == 0:
+        return [html.Li("No files yet!")]
+    else:
+        return [html.Li(file_download_link(filename)) for filename in files]
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True, port=8888)
